b9976a0f87495020397a1718b3a48ac2defc585c
theus-dev <teteuharaujo@gmail.com>
2024-04-17 14:27:11 -0300

    Correção de bugs de bubbleSort.java & criação de info.md

--- Algoritmos de Ordenação/bubbleSort.java
+++ Algoritmos de Ordenação/bubbleSort.java
@@ -1,44 +1,56 @@
-public cass bubbleSort{
+import java.util.Arrays;
+import java.util.Random;
+
+public class bubbleSort {
 
   public static void sort(int[] unsortedArray) {
 
-    
     int length = unsortedArray.length;
-    //Tracking the amount of iterations for Big(O) porpuses
+    // Tracking the amount of iterations for Big(O) porpuses
     int iterations = 0;
 
-    //This variable is responsible for storing the smaller number to be switched
+    // This variable is responsible for storing the smaller number to be switched
     int temporary = 0;
     // Start of the algothm
 
     for (int i = 0; i < length; i++) {
-      for(int j = 1; j < length; j++) {
-        iterations++;
+      for (int j = i + 1; j < length; j++) {
 
-        if(unsortedArray[i] < unsortedArray[j]) {
-          
-          unsortedArray[i] = temporary;
-          unsortedArray[j] = unsortedArray[i];
+        if (unsortedArray[i] > unsortedArray[j]) {
+          iterations++;
+
+          // Switching positions
+          temporary = unsortedArray[i];
+          unsortedArray[i] = unsortedArray[j];
           unsortedArray[j] = temporary;
         }
       }
 
-      
-          
-        
-      }
-    System.out,println("Iterations: " + iterations);
-    
-    
     }
-  public static void main(String[] args) {
-    int[] unsortedArray = {5, 3, 9, 15, 4, 2, 1};
-    sort(unsortedArray);
+    System.out.println("Iterations: " + iterations);
 
-    System.out.println("Sorted Array: "+ Arrays.toString(unsortedArray));
   }
 
+  public static void main(String[] args) {
 
+    // This is a random array generator, you only need to change the length and
+    // maximum number in array.
+
+    int arrayLength = 25; // Change Here as desired
+    int maximumValue = 290; // Change Here as desired
+
+    int[] unsortedArray = new int[arrayLength];
+
+    Random r = new Random();
+    for (int i = 0; i < arrayLength; i++) {
+      unsortedArray[i] = r.nextInt(maximumValue);
+
+    }
+
+    System.out.println("Unsorted Array: " + Arrays.toString(unsortedArray));
+    sort(unsortedArray);
+
+    System.out.println("Sorted Array: " + Arrays.toString(unsortedArray));
+  }
 
 }
-  
